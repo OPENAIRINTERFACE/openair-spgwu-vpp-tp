7e842284af3b8af3a27f09f791075d97c2907354
Andreas Schultz <andreas.schultz@travelping.com>
2019-01-17 12:47:39 +0000

    [FIXUP] fix URR timer handling
    Stop using vlib_time and unix_time at the same time. They are not
    the same and mixing them does not work.
    
    When caluculatin the interval for a timer, use tw->last_run_time as
    base for calculating the time diff. This make sure we always get the
    corret wheel slot for the interval.

--- src/plugins/upf/upf_pfcp_server.h
+++ src/plugins/upf/upf_pfcp_server.h
@@ -97,10 +97,8 @@ extern vlib_node_registration_t sx6_input_node;
 #define UDP_DST_PORT_SX 8805
 
 void upf_pfcp_session_stop_urr_time (urr_time_t * t);
-void upf_pfcp_session_start_stop_urr_time (u32 si, f64 now, urr_time_t * t,
-					   u8 start_it);
-void upf_pfcp_session_start_stop_urr_time_abs (u32 si, f64 now,
-					       urr_time_t * t);
+void upf_pfcp_session_start_stop_urr_time (u32 si, urr_time_t * t, u8 start_it);
+void upf_pfcp_session_start_stop_urr_time_abs (u32 si, urr_time_t * t);
 
 u32 upf_pfcp_server_start_timer (u8 type, u32 id, u32 seconds);
 void upf_pfcp_server_stop_timer (u32 handle);
