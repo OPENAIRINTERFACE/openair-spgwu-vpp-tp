cdd5faad50666ee67868176ef987e8209289cccf
Ivan Shvedunov <ivan4th@gmail.com>
2019-12-06 11:47:58 +0300

    Fix UPF binary API and add dump calls
    Fix string handling in the API. They were being confused with vectors.
    Fix deletion of applications. The hash key was being removed too
    early.
    Add upf_applications_dump and upf_application_l7_rule_dump calls
    to make it possible to do proper sync with the controller.

--- src/plugins/upf/upf_api.c
+++ src/plugins/upf/upf_api.c
@@ -63,7 +63,8 @@
 static void
 setup_message_id_table (upf_main_t * sm, api_main_t * am)
 {
-#define _(id,n,crc)   vl_msg_api_add_msg_name_crc (am, #n  #crc, id + sm->msg_id_base);
+#define _(id,n,crc) \
+  vl_msg_api_add_msg_name_crc (am, #n "_" #crc, id + sm->msg_id_base);
   foreach_vl_msg_name_crc_upf;
 #undef _
 }
@@ -73,7 +74,9 @@ _(UPF_ENABLE_DISABLE, upf_enable_disable) \
 _(UPF_APP_ADD_DEL, upf_app_add_del) \
 _(UPF_APP_IP_RULE_ADD_DEL, upf_app_ip_rule_add_del) \
 _(UPF_APP_L7_RULE_ADD_DEL, upf_app_l7_rule_add_del) \
-_(UPF_APP_FLOW_TIMEOUT_SET, upf_app_flow_timeout_set)
+_(UPF_APP_FLOW_TIMEOUT_SET, upf_app_flow_timeout_set) \
+_(UPF_APPLICATIONS_DUMP, upf_applications_dump) \
+_(UPF_APPLICATION_L7_RULE_DUMP, upf_application_l7_rule_dump)
 
 /* API message handler */
 static void vl_api_upf_enable_disable_t_handler
@@ -96,9 +99,11 @@ vl_api_upf_app_add_del_t_handler (vl_api_upf_app_add_del_t * mp)
   vl_api_upf_app_add_del_reply_t *rmp = NULL;
   upf_main_t *sm = &upf_main;
   int rv = 0;
+  u8 *name = format(0, "%s", mp->name);
 
-  rv = upf_app_add_del (sm, mp->name, (u32) (mp->flags), (int) (mp->is_add));
+  rv = upf_app_add_del (sm, name, (u32) (mp->flags), (int) (mp->is_add));
 
+  vec_free(name);
   REPLY_MACRO (VL_API_UPF_APP_ADD_DEL_REPLY);
 }
 
@@ -109,9 +114,12 @@ static void vl_api_upf_app_ip_rule_add_del_t_handler
   vl_api_upf_app_ip_rule_add_del_reply_t *rmp = NULL;
   upf_main_t *sm = &upf_main;
   int rv = 0;
+  u8 *app = format(0, "%s", mp->app);
 
-  rv = upf_rule_add_del (sm, mp->app, mp->id, (int) (mp->is_add), NULL);
+  // TODO: ip rules aren't implemented yet
+  rv = upf_rule_add_del (sm, app, ntohl(mp->id), (int) (mp->is_add), NULL);
 
+  vec_free(app);
   REPLY_MACRO (VL_API_UPF_APP_IP_RULE_ADD_DEL_REPLY);
 }
 
@@ -122,9 +130,12 @@ static void vl_api_upf_app_l7_rule_add_del_t_handler
   vl_api_upf_app_l7_rule_add_del_reply_t *rmp = NULL;
   upf_main_t *sm = &upf_main;
   int rv = 0;
+  u8 *app = format(0, "%s", mp->app), *regex = format(0, "%s", mp->regex);
 
-  rv = upf_rule_add_del (sm, mp->app, mp->id, (int) (mp->is_add), mp->regex);
+  rv = upf_rule_add_del (sm, app, ntohl(mp->id), (int) (mp->is_add), regex);
 
+  vec_free(app);
+  vec_free(regex);
   REPLY_MACRO (VL_API_UPF_APP_L7_RULE_ADD_DEL_REPLY);
 }
 
@@ -136,9 +147,106 @@ static void vl_api_upf_app_flow_timeout_set_t_handler
   vl_api_upf_app_flow_timeout_set_reply_t *rmp = NULL;
   upf_main_t *sm = &upf_main;
 
-  //rv = upf_flow_timeout_update(mp->type, mp->default_value);
+  //rv = upf_flow_timeout_update(mp->type, ntohs(mp->default_value));
 
   REPLY_MACRO (VL_API_UPF_APP_FLOW_TIMEOUT_SET_REPLY);
+}
+
+static void
+send_upf_applications_details (vl_api_registration_t * reg,
+                               u8 * app_name, u32 flags, u32 context)
+{
+  vl_api_upf_applications_details_t *mp;
+  upf_main_t *sm = &upf_main;
+  u32 name_len;
+
+  mp = vl_msg_api_alloc (sizeof (*mp));
+  clib_memset (mp, 0, sizeof (*mp));
+
+  mp->_vl_msg_id = htons (VL_API_UPF_APPLICATIONS_DETAILS
+			  + sm->msg_id_base);
+  mp->context = context;
+
+  name_len = clib_min(vec_len(app_name) + 1, ARRAY_LEN(mp->name));
+  memcpy (mp->name, app_name, name_len-1);
+  ASSERT (0 == mp->name[name_len - 1]);
+  mp->flags = htonl(flags);
+
+  vl_api_send_msg (reg, (u8 *) mp);
+}
+
+/* API message handler */
+static void vl_api_upf_applications_dump_t_handler
+(vl_api_upf_applications_dump_t * mp)
+{
+  upf_main_t *sm = &upf_main;
+  vl_api_registration_t *reg;
+  upf_adf_app_t *app = NULL;
+
+  reg = vl_api_client_index_to_registration (mp->client_index);
+  if (!reg) {
+    return;
+  }
+
+  /* *INDENT-OFF* */
+  pool_foreach(app, sm->upf_apps,
+    ({
+      send_upf_applications_details (reg, app->name, app->flags, mp->context);
+    }));
+  /* *INDENT-ON* */
+}
+
+static void
+send_upf_application_l7_rule_details (vl_api_registration_t * reg,
+                                      u32 id, u8 * regex, u32 context)
+{
+  vl_api_upf_application_l7_rule_details_t *mp;
+  upf_main_t *sm = &upf_main;
+  u32 regex_len;
+
+  mp = vl_msg_api_alloc (sizeof (*mp));
+  clib_memset (mp, 0, sizeof (*mp));
+
+  mp->_vl_msg_id = htons (VL_API_UPF_APPLICATION_L7_RULE_DETAILS
+			  + sm->msg_id_base);
+  mp->context = context;
+
+  mp->id = htonl(id);
+  regex_len = clib_min(vec_len(regex) + 1, ARRAY_LEN(mp->regex));
+  memcpy (mp->regex, regex, regex_len-1);
+  ASSERT (0 == mp->regex[regex_len - 1]);
+
+  vl_api_send_msg (reg, (u8 *) mp);
+}
+
+/* API message handler */
+static void vl_api_upf_application_l7_rule_dump_t_handler
+(vl_api_upf_application_l7_rule_dump_t * mp)
+{
+  upf_main_t *sm = &upf_main;
+  vl_api_registration_t *reg;
+  upf_adf_app_t *app = NULL;
+  upf_adr_t *rule = NULL;
+  u8 *app_name = format(0, "%s", mp->app);
+  uword *p = NULL;
+
+  reg = vl_api_client_index_to_registration (mp->client_index);
+  if (!reg) {
+    return;
+  }
+
+  p = hash_get_mem (sm->upf_app_by_name, app_name);
+  if (!p)
+    return;
+
+  app = pool_elt_at_index (sm->upf_apps, p[0]);
+
+  /* *INDENT-OFF* */
+  pool_foreach(rule, app->rules,
+               ({
+                 send_upf_application_l7_rule_details (reg, rule->id, rule->regex, mp->context);
+               }));
+  /* *INDENT-ON* */
 }
 
 /* Set up the API message handling tables */
